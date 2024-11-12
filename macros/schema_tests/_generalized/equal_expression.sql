e7c9a33b2cbf5b4586d7958a6690c2813fb850a1
davidc789 <21836507+davidc789@users.noreply.github.com>
2024-11-12 15:00:29 +1100

    sqlserver support

--- macros/schema_tests/_generalized/equal_expression.sql
+++ macros/schema_tests/_generalized/equal_expression.sql
@@ -108,3 +108,63 @@
         expression_difference > {{ tolerance }}
         {% endif %}
 {%- endmacro -%}
+
+{%- macro sqlserver__test_equal_expression(
+                                model,
+                                expression,
+                                compare_model,
+                                compare_expression,
+                                group_by,
+                                compare_group_by,
+                                row_condition,
+                                compare_row_condition,
+                                tolerance,
+                                tolerance_percent) -%}
+
+    {%- set compare_model = model if not compare_model else compare_model -%}
+    {%- set compare_expression = expression if not compare_expression else compare_expression -%}
+    {%- set compare_row_condition = row_condition if not compare_row_condition else compare_row_condition -%}
+    {%- set compare_group_by = group_by if not compare_group_by else compare_group_by -%}
+
+    {%- set n_cols = (group_by|length) if group_by else 0 %}
+    with a as (
+        {{ dbt_expectations.get_select(model, expression, row_condition, group_by) }}
+    ),
+    b as (
+        {{ dbt_expectations.get_select(compare_model, compare_expression, compare_row_condition, compare_group_by) }}
+    ),
+    final as (
+
+        select
+            {% for i in range(1, n_cols + 1) -%}
+            coalesce(a.col_{{ i }}, b.col_{{ i }}) as col_{{ i }},
+            {% endfor %}
+            case when a.expression then 1 else 0 end as expression,
+            case when b.expression then 1 else 0 end as compare_expression,
+            abs(coalesce(case when a.expression then 1 else 0 end, 0) - coalesce(case when b.expression then 1 else 0 end, 0)) as expression_difference,
+            abs(coalesce(case when a.expression then 1 else 0 end, 0) - coalesce(case when b.expression then 1 else 0 end, 0))/
+                nullif(case when a.expression then 1 else 0 end * 1.0, 0) as expression_difference_percent
+        from
+        {% if n_cols > 0 %}
+            a
+            full outer join
+            b on
+            {% for i in range(1, n_cols + 1) -%}
+                a.col_{{ i }} = b.col_{{ i }} {% if not loop.last %}and{% endif %}
+            {% endfor -%}
+        {% else %}
+            a cross join b
+        {% endif %}
+    )
+    -- DEBUG:
+    -- select * from final
+    select
+        *
+    from final
+    where
+        {% if tolerance_percent %}
+        expression_difference_percent > {{ tolerance_percent }}
+        {% else %}
+        expression_difference > {{ tolerance }}
+        {% endif %}
+{%- endmacro -%}
